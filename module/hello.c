2125e921fdcb9333babe3c18bb5e20b189a3b7a9
max7im <nogagorilli@gmail.com>
2026-02-05 15:48:20 +0300

    Writing to file???

--- module/hello.c
+++ module/hello.c
@@ -1,10 +1,15 @@
 
+#include <linux/fcntl.h>
+#include <linux/fs.h>
 #include <linux/init.h> /* Needed for the macros */
 #include <linux/jiffies.h>
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/printk.h> /* Needed for pr_info() */
 #include <linux/timer.h>
 #include <linux/timer_types.h>
+#include <linux/types.h>
+#include <linux/workqueue.h>
+#include <linux/workqueue_types.h>
 
 #define MAX_PATH_LEN 256
 
@@ -24,30 +29,62 @@ MODULE_PARM_DESC(g_filename,
                  "A name of a file in output directory to store logs");
 
 /* globals */
-static const char g_baseDir[] = "/var/temp/test_module";
-static char g_fullPath[MAX_PATH_LEN];
+static const char g_baseDir[] = "/var/tmp/test_module/";
 static struct timer_list g_helloTimer;
+static struct work_struct g_writeWork;
+
+static void writeToFileWork(struct work_struct *w) {
+
+  struct file *filp;
+  loff_t pos = 0;
+  char buf[128];
+  int len;
+  char fullPath[MAX_PATH_LEN];
+  static unsigned int s_counter = 0;
+
+  if (snprintf(fullPath, sizeof(fullPath), "%s%s", g_baseDir, g_filename) >=
+      sizeof(fullPath)) {
+    pr_err("File name exceeds max size: %lu", sizeof(fullPath));
+    return;
+  }
+
+  len = snprintf(buf, sizeof(buf), "Hello from kernel module (%u)\n",
+                 ++s_counter);
+  if (len >= sizeof(buf)) {
+    pr_err("Failed to create hello string");
+    return;
+  }
+
+  filp = filp_open(fullPath, O_WRONLY | O_APPEND | O_CREAT, 0644);
+
+  if (IS_ERR(filp)) {
+    pr_err("Failed to open file: \"%s\"", fullPath);
+    return;
+  }
+
+  kernel_write(filp, buf, len, &pos);
+
+  filp_close(filp, NULL);
+}
 
 static void helloTimerCallback(struct timer_list *t) {
-  static unsigned int counter = 0;
-  pr_info("Hello from kernel module (%u)", ++counter);
-
-  // if (snprintf(g_fullPath, sizeof(g_fullPath), "%s%s", g_baseDir, g_filename)
-  // >=
-  //     sizeof(g_fullPath)) {
-  //   // TODO: handle truncation
-  // }
-
+  schedule_work(&g_writeWork);
   mod_timer(&g_helloTimer, jiffies + msecs_to_jiffies(g_timerPeriodMs));
 }
 
 static int __init init_hello(void) {
+  INIT_WORK(&g_writeWork, &writeToFileWork);
+
   timer_setup(&g_helloTimer, &helloTimerCallback, 0);
   mod_timer(&g_helloTimer, jiffies + msecs_to_jiffies(g_timerPeriodMs));
+
   return 0;
 }
 
-static void __exit cleanup_hello(void) { del_timer_sync(&g_helloTimer); }
+static void __exit cleanup_hello(void) {
+  del_timer_sync(&g_helloTimer);
+  cancel_work_sync(&g_writeWork);
+}
 
 module_init(init_hello);
 module_exit(cleanup_hello);
